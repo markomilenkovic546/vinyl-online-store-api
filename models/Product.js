dda089625bb2adcc531d6c68f67c0d266b19e003
markomilenkovic546 <markomilenkovic546@gmail.com>
2024-09-13 09:54:42 +0200

    Created Product model

---
+++ models/Product.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const trackSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    length: {
+        type: String,
+        required: true
+    }
+});
+
+
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    artist: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    decade: {
+        type: String,
+        required: true
+    },
+    format: {
+        type: String,
+        required: true
+    },
+    coverImage: {
+        type: String,
+        required: true,
+        default: ''
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    tracks: [trackSchema], // Array of tracks
+    inStock: {
+        type: Boolean,
+        default: true 
+    },
+    orderedCount: {
+        type: Number,
+        default: 0
+    },
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review' // Refers to reviews from the Review collection
+    }]
+}, {
+    timestamps: true 
+});
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
