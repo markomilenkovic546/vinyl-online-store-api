a6907ae402e8144ad912f735afacbb3261c4af3d
markomilenkovic546 <markomilenkovic546@gmail.com>
2024-11-04 12:05:35 +0100

    Moved service functions for register and login from userService.js to authService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -10,51 +10,6 @@ import { remove } from 'fs-extra';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const registerUser = async (registerRequestDTO) => {
-    const { firstName, lastName, email, password } = registerRequestDTO;
-    // Encrypt the password
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    // Create a new user object
-    const newUser = new User({
-        firstName,
-        lastName,
-        email,
-        password: passwordHash,
-        addresses: [
-            {
-                firstName: firstName,
-                lastName: lastName,
-                isDefault: true
-            }
-        ]
-    });
-
-    // Record a new user in db
-    const user = await newUser.save();
-    return user;
-};
-
-export const loginUser = async (loginRequestDTO) => {
-    const { email, password } = loginRequestDTO;
-    // Search for email in db
-    const user = await User.findOne({ email });
-    if (!user) {
-        throw new Error('User does not exist.');
-    }
-
-    // Validate password
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-        throw new Error('Password is incorrect.');
-    }
-
-    // Sign the access token
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    return { token, user };
-};
-
 export const getUserById = async (userId) => {
     const user = await User.findById(userId);
     return user;
